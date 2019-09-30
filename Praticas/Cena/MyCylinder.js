c2abd56e295488d3970efa113c84f189c2511082
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2019-09-30 11:21:59 +0100

    Finished cylinder primitive

--- Praticas/Cena/MyCylinder.js
+++ Praticas/Cena/MyCylinder.js
@@ -9,7 +9,7 @@ class MyCylinder extends CGFobject {
         this.stacks = stacks;
         this.height = height;
         this.radius1 = radius1;
-        //this.radius2 = radius2;
+        this.radius2 = radius2;
         this.initBuffers();
     }
     initBuffers() {
@@ -22,11 +22,12 @@ class MyCylinder extends CGFobject {
         var alphaAng = 2 * Math.PI / this.slices;
 
         var stackHeight = this.height / this.stacks; //height of each stack of the cylinder
-        var tempHeight = -this.height / 2; //this variable will save the height of the new stack
+        var tempHeight = 0; //this variable will save the height of the new stack
+ 
+        var radiusDif = this.radius2 - this.radius1;
+        var radiusPerStack = radiusDif / this.stacks;
+        var radiusTemp = this.radius1;
 
-
-        console.log(this.slices);
-        console.log(this.stacks);
 
         for (var j = 0; j < this.stacks; j++) {
         
@@ -34,30 +35,35 @@ class MyCylinder extends CGFobject {
             
             for (var i = 0; i < this.slices; i++) {
 
-                var sa = Math.sin(ang) * this.radius1;
-                var saa = Math.sin(ang + alphaAng) * this.radius1;
-                var ca = Math.cos(ang) * this.radius1;
-                var caa = Math.cos(ang + alphaAng) * this.radius1;
+                var sa = Math.sin(ang) * radiusTemp;
+                var saa = Math.sin(ang + alphaAng) * radiusTemp;
+                var ca = Math.cos(ang) * radiusTemp;
+                var caa = Math.cos(ang + alphaAng) * radiusTemp;
 
-                // vertices of the face of the prism
-                this.vertices.push(ca, sa, tempHeight + stackHeight);
+                var sa2 = Math.sin(ang) * (radiusTemp + radiusPerStack);
+                var saa2 = Math.sin(ang + alphaAng) * (radiusTemp + radiusPerStack);
+                var ca2 = Math.cos(ang) * (radiusTemp + radiusPerStack);
+                var caa2 = Math.cos(ang + alphaAng) * (radiusTemp + radiusPerStack);
+
+                // vertices of the face of the cylinder
+                this.vertices.push(ca2, sa2, tempHeight + stackHeight);
                 this.vertices.push(ca, sa, tempHeight);
-                this.vertices.push(caa, saa, tempHeight + stackHeight);
+                this.vertices.push(caa2, saa2, tempHeight + stackHeight);
                 this.vertices.push(caa, saa, tempHeight);
 
-                // normals of the face of the prism
+                // normals of the face of the cylinder
                 this.normals.push(Math.cos(ang), 0, Math.sin(ang));
                 this.normals.push(Math.cos(ang), 0, Math.sin(ang));
                 this.normals.push(Math.cos(ang + alphaAng), 0, Math.sin(ang + alphaAng));
                 this.normals.push(Math.cos(ang + alphaAng), 0, Math.sin(ang + alphaAng));
 
-                // indices of the face of the prism
-                this.indices.push(4 * i + (this.slices * 2 * j), (4 * i + 1) + (this.slices * 2 * j), (4 * i + 2) + (this.slices * 2 * j));
-                this.indices.push((4 * i + 1) + (this.slices * 2 * j), (4 * i + 3) + (this.slices * 2 * j), (4 * i + 2) + (this.slices * 2 * j));
-                this.indices.push((4 * i + 2) + (this.slices * 2 * j), (4 * i + 1) + (this.slices * 2 * j), 4 * i + (this.slices * 2 * j));
-                this.indices.push((4 * i + 2) + (this.slices * 2 * j), (4 * i + 3) + (this.slices * 2 * j), (4 * i + 1) + (this.slices * 2 * j));
+                // indices of the face of the cylinder
+                this.indices.push(4 * i + (this.slices * 4 * j), (4 * i + 1) + (this.slices * 4 * j), (4 * i + 2) + (this.slices * 4 * j));
+                this.indices.push((4 * i + 1) + (this.slices * 4 * j), (4 * i + 3) + (this.slices * 4 * j), (4 * i + 2) + (this.slices * 4 * j));
+                this.indices.push((4 * i + 2) + (this.slices * 4 * j), (4 * i + 1) + (this.slices * 4 * j), 4 * i + (this.slices * 4 * j));
+                this.indices.push((4 * i + 2) + (this.slices * 4 * j), (4 * i + 3) + (this.slices * 4 * j), (4 * i + 1) + (this.slices * 4 * j));
 
-                // text cords of the face of the prism
+                // text cords of the face of the cylinder
                 this.texCoords.push(i * 1.0 / this.slices, 0);
                 this.texCoords.push(i * 1.0 / this.slices, 1);
                 this.texCoords.push(i * 1.0 / this.slices + 1.0 / this.slices, 0);
@@ -65,12 +71,9 @@ class MyCylinder extends CGFobject {
 
                 ang += alphaAng;
                 
-                console.log(this.vertices.length);
-                console.log(this.normals.length);
-                console.log(this.indices.length);
-                console.log(this.texCoords.length);
             }
-            tempHeight = tempHeight + stackHeight;
+            tempHeight += stackHeight;
+            radiusTemp += radiusPerStack;
             
         }
 
