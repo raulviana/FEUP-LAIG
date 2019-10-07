ef993d347be58bacb09f3a823d5a56f81cbf2d2d
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2019-10-07 16:25:24 +0100

    Sphere and torus final version

--- Praticas/LAIGDemoParser20192020v1.1/MyTorus.js
+++ Praticas/LAIGDemoParser20192020v1.1/MyTorus.js
@@ -22,16 +22,30 @@ class MyTorus extends CGFobject {
         var angLoop = 2 * Math.PI / this.loops;
         var angSlices = 2 * Math.PI / this.slices;
 
-        for (var angY = 0; angY <= 2 * Math.PI + angLoop; angY += angLoop) {
-            for (var angT = 0; angT <= 2 * Math.PI; angT += angSlices) {
+        var texHeight = 1 / this.slices;
+        var texWidth = 1 / this.loops;
+
+        var tempTexHeight;
+        var tempTexWidth = 0;
+
+        // Loop that creates the vertices, normals and texCoords of the Torus
+
+        for (var angY = 0; angY <= 2 * Math.PI + angLoop / 2; angY += angLoop) {
+            tempTexHeight = 1;
+            for (var angT = 0; angT <= 2 * Math.PI + angSlices / 2; angT += angSlices) {
                 this.vertices.push((this.outerRadius + this.innerRadius * Math.cos(angT)) * Math.cos(angY), (this.outerRadius + this.innerRadius * Math.cos(angT)) * Math.sin(angY), this.innerRadius * Math.sin(angT));
                 this.normals.push(Math.cos(angT) * Math.cos(angY), Math.cos(angT) * Math.sin(angY), Math.sin(angT));
+                this.texCoords.push(tempTexWidth + texWidth, tempTexHeight - texHeight);
+                tempTexHeight -= texHeight;
             }
+            tempTexWidth += texWidth;
         }
-      
+
         var nVertices = this.vertices.length / 3 - (this.slices + 1);
 
-        for (var i = 0; i < nVertices ; i++) {
+        // Loop that creates indices for the torus
+
+        for (var i = 0; i < nVertices; i++) {
 
             if ((i + 1) % (this.slices + 1) != 0) {
                 this.indices.push(i, i + this.slices + 1, i + this.slices + 2);
@@ -43,9 +57,7 @@ class MyTorus extends CGFobject {
         }
 
         //this.indices.push(549, );
-        
-        console.log(this.vertices.length / 3);
-        console.log(nVertices);
+
 
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
